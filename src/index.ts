24d310182c2e44c5188adffeb88697759d70a748
Artem Kovtun <rtmkvtn@gmail.com>
2025-05-12 21:43:43 +0800

    feat: add constants.ts

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.post('/telegram-webhook', (req, res) => {
 });
 
 // Start Express server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
