1b17b06855226c96814366843d1952dcb5071c73
Artem Kovtun <rtmkvtn@gmail.com>
2025-05-13 21:52:00 +0800

    update constants imports

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const BOT_TOKEN = process.env.BOT_TOKEN;
 export const DOMAIN = process.env.DOMAIN || 'https://democoffeeandseaguls.ru';
 export const WEBAPP_URL = process.env.WEBAPP_URL || 'https://democoffeeandseaguls.ru/web-app';
